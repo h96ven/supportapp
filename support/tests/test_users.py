4732a5b2881c1f9147f149fbf5525e87fd850a2f
Yevhen Liubarskyi <33.heavens@gmail.com>
2022-08-10 23:59:47 +0300

    moved pytest.ini to root folder

--- support/tests/test_users.py
+++ support/tests/test_users.py
@@ -1,16 +1,31 @@
+import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
 
 
-class TestGetUsers:
-    def test_if_user_is_anonymous_returns_401(self):
-        client = APIClient()
-        response = client.get('/auth/users/')
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+@pytest.mark.django_db
+def test_register_user():
+    pass
+
+# class TestGetUsers:
+#     def test_if_user_is_anonymous_returns_401(self):
+#         client = APIClient()
+#         response = client.get('/auth/users/')
+#         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-class TestGetUsersMe:
-    def test_if_user_is_anonymous_returns_401(self):
-        client = APIClient()
-        response = client.get('/auth/users/me/')
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+# class TestGetUsersMe:
+#     def test_if_user_is_anonymous_returns_401(self):
+#         client = APIClient()
+#         response = client.get('/auth/users/me/')
+#         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+
+
+
+
+
+
+
+
