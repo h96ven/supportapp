df063faae3819eaea03c93bfec38f458c4817c2f
Yevhen Liubarskyi <33.heavens@gmail.com>
2022-08-11 21:55:23 +0300

    created more tests, added tests docker container

--- support/tests/test_users.py
+++ support/tests/test_users.py
@@ -2,30 +2,45 @@ import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
 
+client = APIClient()
+
 
 @pytest.mark.django_db
-def test_register_user():
-    pass
+class TestPostUser:
+    def test_if_register_user_returns_201(self):
+        payload = dict(
+            username='user7',
+            email='user7@domain.com',
+            password='ILoveDjango'
+        )
 
-# class TestGetUsers:
-#     def test_if_user_is_anonymous_returns_401(self):
-#         client = APIClient()
-#         response = client.get('/auth/users/')
-#         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        response = client.post('/auth/users/', payload)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_if_login_user_returns_200(self):
+        payload = dict(
+            username='user7',
+            email='user7@domain.com',
+            password='ILoveDjango'
+        )
+
+        client.post('/auth/users/', payload)
+
+        response = client.post('/auth/jwt/create/',
+                               dict(username='user7',
+                                    password='ILoveDjango'))
+
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_login_user_with_wrong_credentials_returns_401(self):
+        response = client.post('/auth/jwt/create/',
+                               dict(username='user1001',
+                                    password='whfebgwuhfdevbgu'))
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-# class TestGetUsersMe:
-#     def test_if_user_is_anonymous_returns_401(self):
-#         client = APIClient()
-#         response = client.get('/auth/users/me/')
-#         assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-
-
-
-
-
-
-
-
-
+class TestGetUsers:
+    def test_if_user_is_anonymous_returns_401(self):
+        response = client.get('/auth/users/')
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
